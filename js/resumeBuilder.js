496eeb412767d1ee6a9cb35e9e0a59e1900a4449
Christopher Taylor <ctaylor@liquidswords.ninja>
2016-01-26 18:43:01 -0800

    Add data that will populate resume.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,58 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+/* Data */
+var bio = {
+    "name": "Christopher Taylor",
+    "role": "Front-End Web Developer",
+    "welcomeMSG": "A WELCOME MESSAGE GOES HERE",
+    "contact": {
+        "location": "Seattle, WA",
+        "github": "LiquidSwordsman",
+        "email": "ctaylor@liquidswords.ninja",
+        "cell": "206-922-9835"
+        },
+    "imageURL": "liquidswords.ninja/resume/images/pic.jpg",
+    "skills": ["PHP", "HTML", "CSS", "JavaScript", "Git", "C#", "Python 2.X"]
+    };
+var work = [
+    {
+        "employer": null,
+        "title": null,
+        "dates": null,
+        "location": null,
+        "description": null
+    }
+];
+var projects =[
+    {
+        "title": null,
+        "dates": null,
+        "description": null, 
+        "images": []
+    }
+]
+var education = [
+    {
+        "name": "Seattle Central College",
+        "attended": "2013-2015",
+        "location": "Seattle, WA, US",
+        "gpa": 3.2,
+        "degree": "A.A.S.T.",
+        "major": "Programming",
+        "url": "http://www.seattlecentral.edu/",
+        "onlineCourses": {
+            "title":null,
+            "school":null,
+            "dates":null,
+            "url":null
+        }
+    },
+    {
+        "name": "Udacity",
+        "attended": "2015-Present",
+        "location": "Online",
+        "gpa": "Still Attending",
+        "degree": "Nanodegree",
+        "major": "Front-End Web Developer",
+        "url": "https://www.udacity.com/",
+        "onlineCourses": {}
+    }
+];
