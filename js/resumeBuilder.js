dafabc9cf90a328e01a11696c7c8665c3e1ff83c
Christopher Taylor <ctaylor@liquidswords.ninja>
2016-01-27 11:20:35 -0800

    refactor: Clean up global the global namespace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,30 +11,68 @@ var bio = {
         "cell": "206-922-9835"
     },
     "imageURL": "http://liquidswords.ninja/resume/images/pic.jpg",
-    "skills": ["PHP", "HTML", "CSS", "JavaScript", "Git", "C#", "Python 2.X"]
-};
-var work = [
-    {
-        "employer": "Radio Shack",
-        "title": "Sales Associate",
-        "dates": {
-            "start": new Date(2013, 2),
-            "end": new Date(2013, 8)
-        },
-        "location": "Seattle, WA",
-        "description": "This is a job description."
-    },
-    {
-        "employer": "Cameras West",
-        "title": "Sales Associate",
-        "dates": {
-            "start": new Date(2011, 7),
-            "end": new Date(2013, 2)
-        },
-        "location": "Seattle, WA",
-        "description": "This is a job description."
+    "skills": ["PHP", "HTML", "CSS", "JavaScript", "Git", "C#", "Python 2.X"],
+    display: function(){
+        $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
+        $("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+        $("#header").append(HTMLbioPic.replace("%data%", bio.imageURL));
+        $("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMSG));
+        var HTMLcontacts = "";
+        
+        for(var contact in bio.contact){
+            if (bio.contact.hasOwnProperty(contact)){
+                HTMLcontacts += HTMLcontactGeneric.replace("%contact%", contact).replace("%data%", bio.contact[contact]);
+            }
+        }
+        $("#topContacts").append(HTMLcontacts);
+        $("#footerContacts").append(HTMLcontacts);
+
+        if (bio.skills.length > 0) {
+            $("#header").append(HTMLskillsStart);
+
+            bio.skills.forEach(function (skill) {
+                $("#skills").append(HTMLskills.replace("%data%", skill));
+            });
+        }
     }
-];
+};
+var work = {
+    "jobs": [
+        {
+            "employer": "Radio Shack",
+            "title": "Sales Associate",
+            "dates": {
+                "start": new Date(2013, 2),
+                "end": new Date(2013, 8)
+            },
+            "location": "Seattle, WA",
+            "description": "This is a job description."
+        },
+        {
+            "employer": "Cameras West",
+            "title": "Sales Associate",
+            "dates": {
+                "start": new Date(2011, 7),
+                "end": new Date(2013, 2)
+            },
+            "location": "Seattle, WA",
+            "description": "This is a job description."
+        }
+    ],
+    display: function(){
+        this.jobs.forEach(function (job) {
+            $("#workExperience").append(HTMLworkStart);
+            var formattedEmployerTitle = HTMLworkEmployer.replace("%data%", job.employer) + HTMLworkTitle.replace("%data%", job.title);
+            var startDate = job.dates.start.getMonth().toString() + "/" + job.dates.start.getFullYear().toString();
+            var endDate = job.dates.end.getMonth().toString() + "/" + job.dates.end.getFullYear().toString();
+
+            $(".work-entry:last").append(formattedEmployerTitle);
+            $(".work-entry:last").append(HTMLworkDates.replace("%data%", startDate + "-" + endDate));
+            $(".work-entry:last").append(HTMLworkDescription.replace("%data%", job.description));
+            $(".work-entry:last").append(HTMLworkLocation.replace("%data%", job.location));
+        });
+    }
+};
 var projects = [
     {
         "title": null,
@@ -69,44 +107,5 @@ var education = [
     }
 ];
 
-
-bio.display = function() {
-    $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
-    $("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
-    $("#header").append(HTMLbioPic.replace("%data%", bio.imageURL));
-    $("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMSG));
-    var HTMLcontacts = "";
-    
-    for(var contact in bio.contact){
-        if (bio.contact.hasOwnProperty(contact)){
-            HTMLcontacts += HTMLcontactGeneric.replace("%contact%", contact).replace("%data%", bio.contact[contact]);
-        }
-    }
-    $("#topContacts").append(HTMLcontacts);
-    $("#footerContacts").append(HTMLcontacts);
-
-    if (bio.skills.length > 0) {
-        $("#header").append(HTMLskillsStart);
-
-        bio.skills.forEach(function (skill) {
-            $("#skills").append(HTMLskills.replace("%data%", skill));
-        });
-    }
-}
-
-work.forEach(function (job) {
-    $("#workExperience").append(HTMLworkStart);
-        
-        var formattedEmployer = HTMLworkEmployer.replace("%data%", job.employer);
-        var formattedTitle = HTMLworkTitle.replace("%data%", job.title);
-        var formattedEmployerTitle = formattedEmployer + formattedTitle;
-        $(".work-entry:last").append(formattedEmployerTitle);
-        var startDate = job.dates.start.getMonth().toString() + "/" + job.dates.start.getFullYear().toString();
-        var endDate = job.dates.end.getMonth().toString() + "/" + job.dates.end.getFullYear().toString();
-        $(".work-entry:last").append(HTMLworkDates.replace("%data%", startDate + "-" + endDate));
-        $(".work-entry:last").append(HTMLworkDescription.replace("%data%", job.description));
-        $(".work-entry:last").append(HTMLworkLocation.replace("%data%", job.location));
-        
-});
-
 bio.display();
+work.display();
